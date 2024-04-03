e6d114e2efcea8b481f24c05d4c157be46d785ac
Slava Chalnev <slava.chalnev@gmail.com>
2024-04-03 20:50:05 +0800

    Optimise buffer

--- drill/buffer.py
+++ drill/buffer.py
@@ -1,37 +1,35 @@
-import os
-from typing import Any, Iterator, cast
-
 import torch
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from transformer_lens import HookedTransformer
+from drill.config import SAEConfig
 
 
 class ActivationBuffer:
-    def __init__(self, cfg: Any, model: HookedTransformer):
-        self.cfg = cfg
-        self.model = model
+    def __init__(self, cfg: SAEConfig, model: HookedTransformer):
+        self.cfg: SAEConfig = cfg
+        self.model: HookedTransformer = model
         self.dataset = load_dataset(cfg.dataset_path, split="train", streaming=True)
         self.token_loader = DataLoader(self.dataset, batch_size=cfg.store_batch_size, shuffle=True)
-
-        self.buffer = []
         self.buffer_size = cfg.n_batches_in_buffer * cfg.store_batch_size
+        self.buffer = torch.zeros((self.buffer_size, *self.cfg.d_in), dtype=torch.float32)
         self.batch_idx = 0
-    
+
     def fill_buffer(self):
-        while len(self.buffer) < self.buffer_size:
+        buffer_index = self.batch_idx * self.cfg.store_batch_size
+        while buffer_index < self.buffer_size:
             tokens = self.get_token_batch()
             activations = self.model.run_with_cache(tokens, stop_at_layer=self.cfg.hook_point_layer + 1)[1][self.cfg.hook_point]
-            self.buffer.append(activations)
-        # shuffle
-        self.buffer = self.buffer[torch.randperm(len(self.buffer))]
+            self.buffer[buffer_index : buffer_index + self.cfg.store_batch_size] = activations
+            buffer_index += self.cfg.store_batch_size
+        self.buffer = self.buffer[torch.randperm(self.buffer_size)]
         self.batch_idx = 0
 
     def get_activations(self):
-        if self.idx > self.cfg.n_batches_in_buffer // 2:
-            self.buffer = self.buffer[self.batch_idx:]
+        if self.batch_idx > self.cfg.n_batches_in_buffer // 2:
+            self.buffer = self.buffer[self.batch_idx * self.cfg.store_batch_size :]
             self.fill_buffer()
-        activations = torch.stack(self.buffer[:self.cfg.store_batch_size], dim=0)
+        activations = self.buffer[self.batch_idx * self.cfg.store_batch_size : (self.batch_idx + 1) * self.cfg.store_batch_size]
         self.batch_idx += 1
         return activations
 
@@ -41,4 +39,3 @@ class ActivationBuffer:
         except StopIteration:
             self.token_loader = DataLoader(self.dataset, batch_size=self.cfg.store_batch_size, shuffle=True)
             return next(self.token_loader)
-
